a38bc9d32224da5a484c5a3e858f56ab3e54ec04
Sebastian Muszynski <basti@linkt.de>
2025-11-27 17:56:20 +0100

    Add ESPHome 2025.12.0 compatibility (#58)

--- components/basen_bms_ble/basen_bms_ble.cpp
+++ components/basen_bms_ble/basen_bms_ble.cpp
@@ -131,8 +131,7 @@ void BasenBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
       auto *char_notify =
           this->parent_->get_characteristic(BASEN_BMS_SERVICE_UUID, BASEN_BMS_NOTIFY_CHARACTERISTIC_UUID);
       if (char_notify == nullptr) {
-        ESP_LOGE(TAG, "[%s] No notify service found at device, not an BASEN BMS..?",
-                 this->parent_->address_str().c_str());
+        ESP_LOGE(TAG, "[%s] No notify service found at device, not an BASEN BMS..?", this->parent_->address_str());
         break;
       }
       this->char_notify_handle_ = char_notify->handle;
@@ -146,8 +145,7 @@ void BasenBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
       auto *char_command =
           this->parent_->get_characteristic(BASEN_BMS_SERVICE_UUID, BASEN_BMS_CONTROL_CHARACTERISTIC_UUID);
       if (char_command == nullptr) {
-        ESP_LOGE(TAG, "[%s] No control service found at device, not an BASEN BMS..?",
-                 this->parent_->address_str().c_str());
+        ESP_LOGE(TAG, "[%s] No control service found at device, not an BASEN BMS..?", this->parent_->address_str());
         break;
       }
       this->char_command_handle_ = char_command->handle;
@@ -223,7 +221,7 @@ void BasenBmsBle::assemble_(const uint8_t *data, uint16_t length) {
 
 void BasenBmsBle::update() {
   if (this->node_state != espbt::ClientState::ESTABLISHED) {
-    ESP_LOGW(TAG, "[%s] Not connected", this->parent_->address_str().c_str());
+    ESP_LOGW(TAG, "[%s] Not connected", this->parent_->address_str());
     return;
   }
 
@@ -649,7 +647,7 @@ bool BasenBmsBle::send_command_(uint8_t start_of_frame, uint8_t function, uint8_
                                sizeof(frame), frame, ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
 
   if (status) {
-    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str().c_str(), status);
+    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str(), status);
   }
 
   return (status == 0);
