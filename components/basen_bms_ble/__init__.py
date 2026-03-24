8fe205178a90433d2719127cfdb58360d95678ad
Sebastian Muszynski <basti@linkt.de>
2026-03-24 20:43:20 +0100

    Move minimum ESPHome version requirement into validator (#64)

--- components/basen_bms_ble/__init__.py
+++ components/basen_bms_ble/__init__.py
@@ -21,14 +21,15 @@ BASEN_BMS_BLE_COMPONENT_SCHEMA = cv.Schema(
     }
 )
 
-CONFIG_SCHEMA = (
+CONFIG_SCHEMA = cv.All(
+    cv.require_esphome_version(2024, 12, 0),
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(BasenBmsBle),
         }
     )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
-    .extend(cv.polling_component_schema("10s"))
+    .extend(cv.polling_component_schema("10s")),
 )
 
 
