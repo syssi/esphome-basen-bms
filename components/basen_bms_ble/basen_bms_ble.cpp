03a904d749cdf8087583960e49b6d160f4698789
Sebastian Muszynski <basti@linkt.de>
2026-06-10 09:09:19 +0200

    Drop

--- components/basen_bms_ble/basen_bms_ble.cpp
+++ components/basen_bms_ble/basen_bms_ble.cpp
@@ -106,8 +106,6 @@ void BasenBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
     case ESP_GATTC_DISCONNECT_EVT: {
       this->node_state = espbt::ClientState::IDLE;
 
-      // this->publish_state_(this->voltage_sensor_, NAN);
-
       if (this->char_notify_handle_ != 0) {
         auto status = esp_ble_gattc_unregister_for_notify(this->parent()->get_gattc_if(),
                                                           this->parent()->get_remote_bda(), this->char_notify_handle_);
